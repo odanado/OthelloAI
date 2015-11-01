666d74ff4d14b8b2a17506e4047ea412aff99ecd
odan <odan3240@gmail.com>
2015-11-01 15:18:39 +0900

    Config用のテストを追加

---
+++ test/ConfigTest.cpp
@@ -0,0 +1,13 @@
+#include "../Config.hpp"
+#include "gtest/gtest.h"
+
+class ConfigTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+    }
+};
+
+TEST_F(ConfigTest, ConstantValueTest) {
+    ASSERT_EQ(OthelloAI::maxValue<OthelloAI::i64>, -OthelloAI::minValue<OthelloAI::i64>);
+}
+
